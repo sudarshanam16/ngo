e841cef001f05cabf7359fa0caabcaadb15fb3f0
mohak300501 <mohak300501@users.noreply.github.com>
2021-06-17 00:46:32 +0530

    improvise

--- file_upload.php
+++ file_upload.php
@@ -43,7 +43,7 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    $message = "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    $message = "The file '". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). "' has been uploaded.";
   } else {
     $message = "Sorry, there was an error uploading your file.";
   }
