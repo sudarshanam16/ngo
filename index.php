0c7b69a29217accbe6ac67756a4773bf3370f488
mohak300501 <mohak300501@users.noreply.github.com>
2021-06-18 10:47:54 +0530

    update

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ session_start();
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
-    <title>MISSAN FOUNDATION</title>
+    <title>GAKAAY SOLUTIONS</title>
 
 </head>
 
@@ -129,7 +129,7 @@ session_start();
         </div>
     </div>
 
-    <div class="container text-center heading mb-4">
+    <div class="container text-center mb-4">
         <div class="copy">&copy;Copyright GAKAAY SOLUTIONS 2021</div>
     </div>
 
